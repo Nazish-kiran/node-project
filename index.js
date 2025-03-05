198fd9681dadcdad8e6efee9a408a53f1a85e546
NazishKiran <nazishkirancosmos123@gmail.com>
2025-03-06 00:09:24 +0500

    added making files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import fs from 'fs'
+import { log } from "console";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,8 +13,27 @@ app.use(express.static(path.join(import.meta.dirname, "public")));
 
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir(`./files` , (err , files)=>{
+console.log(files);
+res.render("index" , {files : files});
+  })
 });
+app.post("/create", (req, res) => {
+ fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt` , req.body.details , (err)=>{
+res.redirect('/')
+ })
+console.log(req.body);
+
+});
+app.get("/file/:filename", (req, res) => {
+fs.readFile(`./files/${req.params.filename}`, 'utf-8' , (err , fileData)=>{
+res.render("show" , {showData : fileData , showName : req.params.filename.replace('.txt', '')})
+} )
+});
+app.get("/edit/:filename", (req, res) => {
+ res.render("edit")
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
